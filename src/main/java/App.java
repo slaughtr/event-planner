cad4ec166a461cbd547aeb4df6719ff9817786e5
Dallas <dslaughtr@gmail.com>
2017-03-17 10:28:28 -0700

    Add a timer to exit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,26 @@
 import java.io.Console;
+import java.util.Timer;
 
 public class App{
 	public static void main(String[] args) {
 		Console userInput = System.console();
+		boolean continue = false;
 		//ask user if they want to plan a party
-		System.out.println("Hey you wanna party?");
-
+		while(!continue) {
+			System.out.println("Hey you wanna party? [Y]es/[N]o");
+			String wannaParty = userInput.readLine().toLowerCase();
+			if (wannaParty.equals("y")) {
+				System.out.println("Dope!");
+				continue = true;
+			} else if (wannaParty.equals("n")) {
+				System.out.println("Well that's a bummer, dude.");
+				System.out.println("Exiting this fantastic app in 5 seconds...");
+				TimedExit();
+			}
+		}
 		//how many people
 		System.out.println("Hey how many people man");
+		int numPeople = userInput.readLine();
 		//food?
 		System.out.println("Y'all gonna eat?");
 

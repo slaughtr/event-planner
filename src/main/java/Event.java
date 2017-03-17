fa382149cf9dab403152e3b90ba2b7c28ec63628
Dallas <dslaughtr@gmail.com>
2017-03-17 08:24:58 -0700

    Add first passing test

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,5 +3,14 @@ import java.util.List;
 import java.util.Arrays;
 
 public class Event{
+  private int mBaseCost = 100;
+
+  public Event() {
+    mBaseCost = mBaseCost;
+  }
+
+  public int getCost() {
+    return mBaseCost;
+  }
 
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -3,4 +3,11 @@ import static org.junit.Assert.*;
 
 public class EventTest {
 
+  @Test
+  public void newParty_hasBaseCost() {
+    Event testEvent = new Event();
+    int baseCost = 100;
+    assertEquals(baseCost, testEvent.getCost());
+  }
+
 }

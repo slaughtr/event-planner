a0213c17bdec7d8984a4567ea762a60879b30c72
Dallas <dslaughtr@gmail.com>
2017-03-17 10:14:51 -0700

    Add boilerplate printlns to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,17 +2,28 @@ import java.io.Console;
 
 public class App{
 	public static void main(String[] args) {
+		Console userInput = System.console();
 		//ask user if they want to plan a party
+		System.out.println("Hey you wanna party?");
 
 		//how many people
-
+		System.out.println("Hey how many people man");
 		//food?
+		System.out.println("Y'all gonna eat?");
 
 		//drink?
+		System.out.println("What about drinks?");
 
 		//entertainment?
+		System.out.println("I know a guy...");
+
+		//offer coupon codes
+		// if (selectedthingsright) {
+		// 	System.out.println("Hey looks like you're up for some coupons.");
+		// }
 
 		//print final price
+		System.out.println("That'll cost you: ");
 
 	}
 }
